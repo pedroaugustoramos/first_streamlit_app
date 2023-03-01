916e16b141522b3a105b16feb82b6f022d74ea2c
Pedro A A O Ramos <94549913+pedroaugustoramos@users.noreply.github.com>
2023-03-01 17:33:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+stremlit.text("Omega 3 and Bleuberry Oatmeal")
+stremlit.text("Kale, Spinach and Rocket Smoothie")
+stremlit.text("Hard Boiled Free Ranged Egg")
